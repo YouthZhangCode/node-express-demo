f55ac2438f356186c3838a9b655d9694c758a289
YouthZhangCode <coderzhangyang@163.com>
2018-05-13 16:34:29 +0800

    解决引用静态js、css问题

--- node-express-day1/js/hellowrold.js
+++ node-express-day1/js/hellowrold.js
@@ -1,11 +1,15 @@
 var express = require('express')
 var app = express()
+var path = require('path')
+
+app.use(express.static(path.join(__dirname, 'view')))
+// app.use(express.static('view'))
 
 app.get('/index.html', (req, res) => {
   res.sendFile(__dirname + '/' + 'index.html')
 })
 
-var server = app.listen(8081, () => {
+var server = app.listen(8094, () => {
   var host = server.address().address
   var port = server.address().port
 
